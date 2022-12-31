d4f71cf5d24e93fe3167a69ac668a12527b5362a
Abdul Moiz Anwer <89318559+AbdulMoizAnwer@users.noreply.github.com>
2022-12-31 18:30:32 +0500

    Create index3.php

---
+++ index3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practice PHP</title>
+</head>
+<body>
+    <form action="backend3.php" method="POST">
+        <label>Enter your name</label>
+        <input type="text" name="m1">
+        <input type="submit">
+    </form>
+</body>
+</html>
